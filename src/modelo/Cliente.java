8a7799f087929aa0abe8b75070bebec24a8c78fe
Torrijoss <127211463+Torrijoss@users.noreply.github.com>
2025-11-18 12:46:14 +0100

    eterter

--- src/modelo/Cliente.java
+++ src/modelo/Cliente.java
@@ -3,11 +3,13 @@ package modelo;
 public class Cliente extends Usuario{
 	
 	private int pedido;
+	private int tiros;
 
 	public Cliente(String nombre, String apellidos, String correo, String contra) {
 		super(nombre, apellidos, correo, contra);
 		this.admin = false;
 		this.pedido = 0;
+		this.tiros = 1;
 	}
 
 	public int getPedido() {
@@ -16,15 +18,6 @@ public class Cliente extends Usuario{
 
 	public void setPedido(int pedido) {
 		this.pedido = pedido;
-	}
-
-	@Override
-	public String toString() {
-		return "Cliente [pedido=" + pedido + ", Nombre=" + Nombre + ", Apellidos=" + Apellidos + ", Correo=" + Correo
-				+ ", Contra=" + Contra + ", admin=" + admin + ", getPedido()=" + getPedido() + ", getNombre()="
-				+ getNombre() + ", getApellidos()=" + getApellidos() + ", getCorreo()=" + getCorreo() + ", getContra()="
-				+ getContra() + ", isAdmin()=" + isAdmin() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode()
-				+ ", toString()=" + super.toString() + "]";
 	}
 
 	
--- src/vista/Ruleta.java
+++ src/vista/Ruleta.java
@@ -1,16 +1,31 @@
 package vista;
 
 import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
 
 public class Ruleta extends JPanel {
 
 	private static final long serialVersionUID = 1L;
+	public JPanel panel;
 
 
 
 	
 	public Ruleta() {
+		setBackground(new Color(255, 128, 64));
+		setBorder(new LineBorder(new Color(0, 0, 0), 3));
+		setLayout(null);
+		
+		panel = new JPanel();
+		panel.setBorder(new LineBorder(new Color(255, 255, 0), 2));
+		panel.setBackground(new Color(255, 255, 128));
+		panel.setBounds(49, 30, 501, 185);
+		add(panel);
+		
+
 
 	}
-
 }
