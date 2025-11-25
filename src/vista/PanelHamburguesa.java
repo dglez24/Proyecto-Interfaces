3fc4512347579d9b9de815e1ec8edea8b54ab953
Torrijoss <127211463+Torrijoss@users.noreply.github.com>
2025-11-25 09:22:03 +0100

    fdg

--- src/vista/PanelHamburguesa.java
+++ src/vista/PanelHamburguesa.java
@@ -23,52 +23,68 @@ public class PanelHamburguesa extends JPanel {
 	public JLabel nom2;
 	public JLabel nom3;
 	public JButton BtnAtras;
+	public JPanel panel;
+	public JPanel panel_1;
 
 	/**
 	 * Create the panel.
 	 */
 	public PanelHamburguesa() {
-		setBackground(new Color(255, 128, 0));
+		setBackground(new Color(128, 64, 0));
 		setLayout(null);
 		
-		img1 = new JLabel("imagen");
-		img1.setHorizontalAlignment(SwingConstants.CENTER);
-		img1.setBounds(30, 53, 181, 181);
-		add(img1);
 		
-		separator = new JSeparator();
-		separator.setBounds(10, 250, 840, 8);
-		add(separator);
-		
-		img3 = new JLabel("imagen");
-		img3.setHorizontalAlignment(SwingConstants.CENTER);
-		img3.setBounds(623, 53, 181, 181);
-		add(img3);
-		
-		img2 = new JLabel("imagen");
-		img2.setHorizontalAlignment(SwingConstants.CENTER);
-		img2.setBounds(289, 278, 181, 174);
-		add(img2);
-		
-		nom1 = new JLabel("Texto prod 1");
-		nom1.setForeground(new Color(255, 255, 255));
-		nom1.setHorizontalAlignment(SwingConstants.LEFT);
-		nom1.setBounds(30, 268, 181, 120);
-		add(nom1);
-		
-		nom2 = new JLabel("Texto prod 2");
-		nom2.setForeground(new Color(255, 255, 255));
-		nom2.setBounds(339, 114, 121, 120);
-		add(nom2);
-		
-		nom3 = new JLabel("Texto prod 3");
-		nom3.setForeground(new Color(255, 255, 255));
-		nom3.setBounds(683, 268, 121, 120);
-		add(nom3);
 		
 		BtnAtras = new JButton("Atras");
 		BtnAtras.setBounds(766, 10, 84, 20);
 		add(BtnAtras);
+		
+		panel = new JPanel();
+		panel.setBackground(new Color(255, 255, 128));
+		panel.setBounds(0, 42, 859, 400);
+		add(panel);
+		panel.setLayout(null);
+		
+		panel_1 = new JPanel();
+		panel_1.setBackground(new Color(255, 128, 64));
+		panel_1.setBounds(0, 22, 859, 357);
+		panel.add(panel_1);
+		panel_1.setLayout(null);
+		
+		img1 = new JLabel("imagen");
+		img1.setHorizontalAlignment(SwingConstants.CENTER);
+		img1.setBounds(119, 98, 34, 13);
+		panel_1.add(img1);
+		
+		separator = new JSeparator();
+		separator.setBounds(315, 10, 0, 2);
+		panel_1.add(separator);
+		
+		img3 = new JLabel("imagen");
+		img3.setHorizontalAlignment(SwingConstants.CENTER);
+		img3.setBounds(315, 115, 34, 13);
+		panel_1.add(img3);
+		
+		img2 = new JLabel("imagen");
+		img2.setHorizontalAlignment(SwingConstants.CENTER);
+		img2.setBounds(465, 115, 34, 13);
+		panel_1.add(img2);
+		
+		nom1 = new JLabel("Texto prod 1");
+		nom1.setForeground(new Color(255, 255, 255));
+		nom1.setHorizontalAlignment(SwingConstants.LEFT);
+		nom1.setBounds(106, 147, 58, 13);
+		panel_1.add(nom1);
+		
+		nom2 = new JLabel("Texto prod 2");
+		nom2.setForeground(new Color(255, 255, 255));
+		nom2.setBounds(446, 68, 58, 13);
+		panel_1.add(nom2);
+		
+		nom3 = new JLabel("Texto prod 3");
+		nom3.setForeground(new Color(255, 255, 255));
+		nom3.setBounds(551, 98, 58, 13);
+		panel_1.add(nom3);
 
 	}
 }
