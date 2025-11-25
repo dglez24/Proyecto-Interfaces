335dfd5b0c287e623e43a06b79b6d8f4fd1c60d8
Torrijoss <127211463+Torrijoss@users.noreply.github.com>
2025-11-25 09:42:16 +0100

    fdgsd

--- src/vista/PanelLista.java
+++ src/vista/PanelLista.java
@@ -2,12 +2,15 @@ package vista;
 
 import javax.swing.JPanel;
 import java.awt.Color;
+import javax.swing.JButton;
+import java.awt.Font;
 
 public class PanelLista extends JPanel {
 
 	private static final long serialVersionUID = 1L;
 	public JPanel panel;
 	public JPanel panel_1;
+	public JButton btnAtras;
 
 	/**
 	 * Create the panel.
@@ -26,7 +29,12 @@ public class PanelLista extends JPanel {
 		panel_1.setBackground(new Color(255, 128, 64));
 		panel_1.setBounds(0, 22, 859, 357);
 		panel.add(panel_1);
+		
+		btnAtras = new JButton("Atras");
+		btnAtras.setFont(new Font("Tahoma", Font.BOLD, 10));
+		btnAtras.setBackground(new Color(187, 0, 0));
+		btnAtras.setBounds(768, 10, 84, 20);
+		add(btnAtras);
 
 	}
-
 }
