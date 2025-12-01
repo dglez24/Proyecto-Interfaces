6bb5463d256321cef26a66a1bf5c1c857cc4a2a3
Torrijoss <127211463+Torrijoss@users.noreply.github.com>
2025-12-01 11:14:48 +0100

    gtrhtr

--- src/vista/PanelLista.java
+++ src/vista/PanelLista.java
@@ -7,12 +7,21 @@ import java.awt.Font;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import javax.swing.border.LineBorder;
+import javax.swing.JScrollPane;
+import javax.swing.JList;
 
 public class PanelLista extends JPanel {
 
 	private static final long serialVersionUID = 1L;
 	public JPanel panel;
 	public JButton btnAtras;
+	public JScrollPane scrollPane;
+	public JList list;
+	public JButton BTNRestar;
+	public JButton BTNSumar;
+	public JPanel PanelUds;
+	public JLabel LblUds;
+	public JLabel LblTituloUds;
 
 	/**
 	 * Create the panel.
@@ -26,6 +35,48 @@ public class PanelLista extends JPanel {
 		panel.setBounds(0, 42, 859, 400);
 		add(panel);
 		panel.setLayout(null);
+		
+		scrollPane = new JScrollPane();
+		scrollPane.setBounds(128, 35, 399, 331);
+		panel.add(scrollPane);
+		
+		list = new JList();
+		scrollPane.setViewportView(list);
+		
+		BTNRestar = new JButton("-");
+		BTNRestar.setForeground(new Color(255, 255, 255));
+		BTNRestar.setFont(new Font("Tahoma", Font.BOLD, 30));
+		BTNRestar.setBackground(new Color(0, 0, 0));
+		BTNRestar.setBounds(34, 101, 84, 56);
+		panel.add(BTNRestar);
+		
+		BTNSumar = new JButton("+");
+		BTNSumar.setForeground(new Color(255, 255, 255));
+		BTNSumar.setFont(new Font("Tahoma", Font.BOLD, 30));
+		BTNSumar.setBackground(new Color(0, 0, 0));
+		BTNSumar.setBounds(34, 35, 84, 56);
+		panel.add(BTNSumar);
+		
+		PanelUds = new JPanel();
+		PanelUds.setBorder(new LineBorder(new Color(255, 255, 255)));
+		PanelUds.setBackground(new Color(0, 0, 0));
+		PanelUds.setBounds(34, 246, 84, 120);
+		panel.add(PanelUds);
+		PanelUds.setLayout(null);
+		
+		LblUds = new JLabel("20");
+		LblUds.setFont(new Font("Tahoma", Font.BOLD, 30));
+		LblUds.setHorizontalAlignment(SwingConstants.CENTER);
+		LblUds.setBounds(10, 6, 64, 104);
+		LblUds.setForeground(new Color(255, 255, 255));
+		PanelUds.add(LblUds);
+		
+		LblTituloUds = new JLabel("Uds");
+		LblTituloUds.setFont(new Font("Tahoma", Font.BOLD, 30));
+		LblTituloUds.setForeground(new Color(255, 255, 255));
+		LblTituloUds.setHorizontalAlignment(SwingConstants.CENTER);
+		LblTituloUds.setBounds(34, 202, 84, 34);
+		panel.add(LblTituloUds);
 		
 		btnAtras = new JButton("Atras");
 		btnAtras.setFont(new Font("Tahoma", Font.BOLD, 10));
